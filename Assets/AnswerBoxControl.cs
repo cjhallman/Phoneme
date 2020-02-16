ab6484fe0e9f3fd1aee7b6f53fbffae52e3fa892
Connor Hallman <challman@umich.edu>
2020-02-16 15:56:23 -0800

    Build 1

---
+++ Assets/AnswerBoxControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerBoxControl : MonoBehaviour
+{
+    public bool correct = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetText(string t)
+    {
+        GetComponentInChildren<UnityEngine.UI.Text>().text = t;
+    }
+
+    public string GetText()
+    {
+        return GetComponentInChildren<UnityEngine.UI.Text>().text;
+    }
+}
